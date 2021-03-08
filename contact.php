0d16f9d24a8c3d6261d4a0ad6425a58ba276b6b0
Sarikavidya <71083930+Sarikavidya@users.noreply.github.com>
2021-03-08 18:29:19 +0530

    initial commit

--- contact.php
+++ contact.php
@@ -1,4 +1,13 @@
 <?php
+$GithubRawURI= "https://raw.githubusercontent.com/Sarikavidya/myportfolio/contact.php”; 
+$ch = curl_init(); 
+curl_setopt($ch, CURLOPT_URL, $GithubRawURI); 
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+$data = curl_exec($ch); 
+curl_close($ch); 
+ 
+
 use PHPMailer\PHPMailer\PHPMailer;
 
 require_once 'phpmailer/Exception.php';
